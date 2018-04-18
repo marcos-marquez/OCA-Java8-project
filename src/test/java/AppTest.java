624d0f5c503027df4e03f4deef298b615585dc5b
Marcos Marquez <marcos.ipn23@gmail.com>
2018-04-17 22:21:38 -0500

    Es la clase test implementada

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test 
+    public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertEquals(9, 25 - 5 * 4 / 2 - 10 + 4);
+        
     }
 }
